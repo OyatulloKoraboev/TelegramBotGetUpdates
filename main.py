4323bdf0ba052e2b3ee7d87b577489152c9b180c
Koraboev Oyatullo <korabeov.o@gmail.com>
2021-01-31 16:58:14 +0500

    getUpdates

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import requests
 import json
-
+from pprint import pprint
 
 token = '1693418503:AAEcpgl164M2nl0HXzwk0EbnPqnttI3gzmQ'
-url = f'{token}'
+url = f'https://api.telegram.org/bot{token}/getUpdates'
+r = requests.get(url)
+data = r.json()
+pprint(data)
+updates = data['result']
+print(updates)
